04418e93a016e367fc1a97fed5a9159095f5df45
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-01-11 08:34:07 -0800

    css file added

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function search(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#searchform-input");
+    let submitInput = document.querySelector("#submitform-input");
+    let searchInputValue = searchInput.value;
+    let h1 = document.querySelector("#place");
+    h1.innerHTML = searchInputValue;
+}
+let form = document.querySelector(".form-input");
+console.log(form);
+form.addEventListener("submit", search);
+
+function displayWeather() {
+    let city = "lisbon";
+    let apikey = "03 c85048t924o90fc221bd5189a06dda ";
+
+}
+
+
+let apiUrl = "https: //api.shecodes.io/weather/v1/current?query={query}&key={key}"
+axios.get(apiUrl).then(displayWeather)
