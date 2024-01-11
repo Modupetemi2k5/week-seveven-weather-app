206a658e8dd09789f22e807abbf5438a958d141d
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-01-11 10:12:40 -0800

    update axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+function displayWeather(response) {
+  console.log(response.data);
+  let apikey = "03 c85048t924o90fc221bd5189a06dda ";
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchform-input");
@@ -7,15 +12,8 @@ function search(event) {
   h1.innerHTML = searchInputValue;
 }
 let form = document.querySelector(".form-input");
-console.log(form);
 form.addEventListener("submit", search);
 
-function displayWeather(response) {
-  event.preventDefault();
+let apiUrl = ` https: //api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
 
-  let apikey = "03 c85048t924o90fc221bd5189a06dda ";
-}
-
-let apiUrl =
-  "https: //api.shecodes.io/weather/v1/current?query={query}&key={key}";
 axios.get(apiUrl).then(displayWeather);
