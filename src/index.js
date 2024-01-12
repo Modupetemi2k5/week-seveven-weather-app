8b85400dca889182f7a4b65867ada1ed117f43f7
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-01-12 03:00:15 -0800

    js file updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function displayWeather(response) {
   console.log(response.data);
-  let apikey = "03 c85048t924o90fc221bd5189a06dda ";
+  let tempElement = document.querySelector(".temp");
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
 }
 
 function search(event) {
@@ -10,10 +11,10 @@ function search(event) {
   let searchInputValue = searchInput.value;
   let h1 = document.querySelector("#place");
   h1.innerHTML = searchInputValue;
+  let apikey = "03 c85048t924o90fc221bd5189a06dda ";
+  let apiUrl = ` https: //api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&unit=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
 }
 let form = document.querySelector(".form-input");
 form.addEventListener("submit", search);
-
-let apiUrl = ` https: //api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
-
-axios.get(apiUrl).then(displayWeather);
