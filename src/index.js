905df860d2abdb5864ee8595c408bf289f364ea1
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-01-13 21:49:34 -0800

    updated the cloud element

--- src/index.js
+++ src/index.js
@@ -6,19 +6,22 @@ function displayWeather(response) {
   windElement.innerHTML = response.data.wind.speed;
   humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.temperature.humidity;
-
-  function search(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#searchform-input");
-    let submitInput = document.querySelector("#submitform-input");
-    let searchInputValue = searchInput.value;
-    let h1 = document.querySelector("#place");
-    h1.innerHTML = searchInputValue;
-    let apikey = "fbef01f4et1b02o0d25c27210a43ef3f";
-
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputValue}&key=${apikey}`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-  let form = document.querySelector(".form-input");
-  form.addEventListener("submit", search);
+  cloudElement = document.querySelector(".cloud");
+  cloudElement.innerHTML = `<img src = "${response.data.condition.icon_url}"`;
 }
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchform-input");
+  let submitInput = document.querySelector("#submitform-input");
+  let searchInputValue = searchInput.value;
+  let h1 = document.querySelector("#place");
+  h1.innerHTML = searchInputValue;
+  let apikey = "fbef01f4et1b02o0d25c27210a43ef3f";
+
+  let apiUrl = `
+    https: //api.shecodes.io/weather/v1/current?query=${searchInputValue}&key=${apikey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+let form = document.querySelector(".form-input");
+form.addEventListener("submit", search);
