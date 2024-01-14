47abe0df93bd9520ce818157aa932f62fc75ae98
Modupetemi2k5 <151980601+Modupetemi2k5@users.noreply.github.com>
2024-01-13 21:21:59 -0800

    updating  the weather

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ function displayWeather(response) {
   console.log(response.data);
   let tempElement = document.querySelector(".temp");
   tempElement.innerHTML = Math.round(response.data.temperature.current);
-}
+  windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
 
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#searchform-input");
-  let submitInput = document.querySelector("#submitform-input");
-  let searchInputValue = searchInput.value;
-  let h1 = document.querySelector("#place");
-  h1.innerHTML = searchInputValue;
-  let apikey = "fbef01f4et1b02o0d25c27210a43ef3f";
+  function search(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#searchform-input");
+    let submitInput = document.querySelector("#submitform-input");
+    let searchInputValue = searchInput.value;
+    let h1 = document.querySelector("#place");
+    h1.innerHTML = searchInputValue;
+    let apikey = "fbef01f4et1b02o0d25c27210a43ef3f";
 
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputValue}&key=${apikey}`;
-  axios.get(apiUrl).then(displayWeather);
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputValue}&key=${apikey}`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+  let form = document.querySelector(".form-input");
+  form.addEventListener("submit", search);
 }
-let form = document.querySelector(".form-input");
-form.addEventListener("submit", search);
